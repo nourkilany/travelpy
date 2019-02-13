ec433d4e4fa645c0e4373348e326eed62fee00eb
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-13 19:17:24 +0200

    link apps routing with apps view without templates

--- travelPy/urls.py
+++ travelPy/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+# to keep urls project clean use
+# url(r'^blog/', include('travelPyUserBlog.url'))
+# then create url.py with your app routing -> check -> travelPyUserBlog/url.py
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('travelPyUserBlog.url')),
+    url(r'^hotelBooking/', include('travelPyHotelBooking.url')),
+
 ]
