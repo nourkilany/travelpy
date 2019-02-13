119d311516c8f084f560c510d3d010336465842c
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-02-13 15:24:04 +0200

    Add DB Settings

--- travelPy/settings.py
+++ travelPy/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'travelPy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'travelpydb',
+        'USER': 'root',
+        'PASSWORD':'Pa55wod_',
+        # 'PASSWORD': '',
+        # 'PASSWORD': '',
+        # 'PASSWORD': ' ',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
