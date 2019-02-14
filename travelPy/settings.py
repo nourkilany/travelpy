4e0f1a96bea23b7ed4a973679cbcb0a1b045c2c3
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-14 12:10:57 +0200

    static dir with it's settings

--- travelPy/settings.py
+++ travelPy/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# Static Directory
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -27,9 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Templates directory
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Application definition
@@ -135,3 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
