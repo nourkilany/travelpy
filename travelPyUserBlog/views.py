2401ee6e96232eae265ba59cf369108e31ea1521
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-15 17:54:30 +0200

    iterator with forloop

--- travelPyUserBlog/views.py
+++ travelPyUserBlog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import requests
+
 from django.http import HttpResponse
 
 from travelPyUserBlog.models import Cities
@@ -12,8 +14,7 @@ def index(request):
     return render(request, 'travelPyUserBlog/index.html')
 
 
-def cities(request):
-
-    cities_list = Cities.objects.filter(cityCountry='DE')
+def cities(request, country_name):
+    cities_list = Cities.objects.filter(cityCountry=country_name).order_by('cityName').iterator()
     cities_dict = {'cities': cities_list}
     return render(request, 'travelPyUserBlog/cities.html', context=cities_dict)
