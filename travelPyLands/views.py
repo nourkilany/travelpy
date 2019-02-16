3dabeb2d4e30ba5821a81af1a397e0c34058889e
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-17 01:15:37 +0200

    add top cities api logic in views with and it's template

--- travelPyLands/views.py
+++ travelPyLands/views.py
@@ -5,19 +5,20 @@ import requests
 # Create your views here.
 
 
-# https://api.sygictravelapi.com/1.1/en/places/list?levels=city&limit=6 -> cities limit 6
-
-# https://api.sygictravelapi.com/1.1/en/places/list?levels=country&limit=6 -> country limit 6
-
 api_token = 'XCAO5wKKj26IL5oEdfuwY1TpxjOxnIWTfGW4cik4'
 
+
+# send data from api to be rendered in index.html
+
 def home(request):
-    # return HttpResponse('Hello Index!')
-    # api_token = 'XCAO5wKKj26IL5oEdfuwY1TpxjOxnIWTfGW4cik4'
 
-    countriesData_dict = {'topCountries': getCountriesDataApi()}
-    return render(request, "travelPyLands/index.html", context=countriesData_dict)
+    indexData_dict = {'topCountries': getTopCountriesDataApi(),
+                      'topCities': getTopCitiesDataApi()
+                      }
+    return render(request, "travelPyLands/index.html", context=indexData_dict)
 
+
+# get one image from api media with any id
 def getImageFromApi(id):
     response = requests.get(f'https://api.sygictravelapi.com/1.1/en/places/{id}/media',
                             None, headers={
@@ -26,8 +27,10 @@ def getImageFromApi(id):
     imagesData = response.json()
     return imagesData["data"]["media"][0]["url"]
 
-def getCountriesDataApi():
-    countriesResponse = requests.get('https://api.sygictravelapi.com/1.1/en/places/list?levels=country',
+
+# get top countries from api and return list of them
+def getTopCountriesDataApi():
+    countriesResponse = requests.get('https://api.sygictravelapi.com/1.1/en/places/list?levels=country&limit=6',
                                      None, headers={
             'x-api-key': api_token
         })
@@ -35,6 +38,17 @@ def getCountriesDataApi():
     countriesList = addImagesToList(countriesData["data"]["places"])
     return countriesList
 
+# get top cities from api and return list of them
+def getTopCitiesDataApi():
+    citiesResponse = requests.get('https://api.sygictravelapi.com/1.1/en/places/list?levels=city&limit=6',
+                                     None, headers={
+            'x-api-key': api_token
+        })
+    citiesData = citiesResponse.json()
+    citiesList = addImagesToList(citiesData["data"]["places"])
+    return citiesList
+
+# add image  from api to any list of dict with image-> as a key and return list with that image
 def addImagesToList(lst):
     for items in lst:
         items["image"] = getImageFromApi(items['id'])
