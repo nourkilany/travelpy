865b1c9b0bc8a21a6897b823c6b4421c2967117d
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-16 21:15:29 +0200

    api with view

--- travelPyLands/views.py
+++ travelPyLands/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('Hello Index!')
     return render(request, "travelPyLands/index.html")
+
+# https://api.sygictravelapi.com/1.1/en/places/list?levels=city&limit=6 -> cities limit 6
+
+# https://api.sygictravelapi.com/1.1/en/places/list?levels=country&limit=6 -> country limit 6
