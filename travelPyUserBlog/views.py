4492d95d350af519254cad29c6c9456a1416ad3a
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-02-16 18:16:51 +0200

    add template

--- travelPyUserBlog/views.py
+++ travelPyUserBlog/views.py
@@ -14,8 +14,6 @@ import json
 def index(request):
 
     api_token = 'XCAO5wKKj26IL5oEdfuwY1TpxjOxnIWTfGW4cik4'
-    api_key = 'x-api-key'
-    auth_values = (api_key, api_token)
     response = requests.get('https://api.sygictravelapi.com/1.1/en/places/list?levels=city',
                             None, headers={
             'x-api-key': api_token
@@ -30,3 +28,4 @@ def cities(request, country_name):
     cities_list = Cities.objects.filter(cityCountry=country_name).order_by('cityName').iterator()
     cities_dict = {'cities': cities_list}
     return render(request, 'travelPyUserBlog/cities.html', context=cities_dict)
+
