985e690f47ac1fd182229faf7180066449d87aa7
Nour <nour.kilany88@gmail.com>
2019-02-16 23:35:48 +0200

    NavBar fixes and footer minor details

--- travelPy/settings.py
+++ travelPy/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'travelpydb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'wallbase1qaz',
         'HOST': 'localhost',
         'PORT': '3306'
     }
