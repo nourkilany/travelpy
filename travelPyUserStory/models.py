73ae529252cdcad81612a7d97c551830a7fe7d9e
a-tarek <a.tarek.elsayed@gmail.com>
2019-02-20 08:51:00 +0200

    Edit Story Routing

--- travelPyUserStory/models.py
+++ travelPyUserStory/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from travelPyLands.models import City
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Story(models.Model):
     story_title = models.CharField(max_length = 150)
     story_body = models.TextField()
-    user_id  = models.IntegerField()
     city = models.ForeignKey('travelPyLands.City', on_delete=models.CASCADE)
+    user  = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
