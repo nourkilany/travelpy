d795079666c312412b5140a8fcb2ff1423a07d09
Nour <nour.kilany88@gmail.com>
2019-02-20 17:50:17 +0200

    car rent form handling

--- travelPyCarRent/forms.py
+++ travelPyCarRent/forms.py
@@ -1,8 +1,9 @@
-# from django import forms
-# from .models import CarRentalForm
-#
-#
-# class DateForm(forms.ModelForm):
-#     class Meta:
-#         model = CarRentalForm
-#         fields = ('Pickup', 'from', 'to')
+from django import forms
+from .models import CarRental
+
+
+class CarRentForm(forms.ModelForm):
+    class Meta:
+        model = CarRental
+        fields = ('pickup_location', 'from_date', 'to_date')
+        widgets = {'pickup_location': forms.Select( attrs={'class': 'destination search_input input-location'})}
--- travelPyCarRent/views.py
+++ travelPyCarRent/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import CarRental
+from .forms import CarRentForm
 # Create your views here.
 
 
-def index(request):
-
-    return render(request, 'travelPyCarRent/index.html')
+def new_car_rent(request):
+    form = CarRentForm()
+    if request.method =="POST":
+        form = CarRentForm(request.POST)
+        if form.is_valid():
+            car_request = CarRental.objects.create(
+                pickup_location = request.POST.get("pickup_location"),
+                from_date  = request.POST.get('from_date'),
+                to_date  = request.POST.get('to_date'),
+                user_id     = 100,   # user_id ->
+                )
+    return render(request, 'travelPyCarRent/index.html',{'form':form})
