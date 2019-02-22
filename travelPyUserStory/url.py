d15f9ef9cc48d08253550fc543152dbd6ff4e702
a-tarek <a.tarek.elsayed@gmail.com>
2019-02-22 14:27:47 +0200

    Modify urls

--- travelPyUserStory/url.py
+++ travelPyUserStory/url.py
@@ -5,9 +5,10 @@ from django.conf.urls import url
 from travelPyUserStory import views
 
 urlpatterns = [
-    url(r'^$', views.all),
-    url(r'^new/$', views.new_story),
+    url(r'^city_id=(?P<city_id>\d+)/$', views.all),
+    url(r'^new/city_id=(?P<city_id>\d+)/$', views.new_story),
     url(r'^edit/(?P<story_id>\d+)/$', views.edit_story),
     url(r'^(?P<story_id>\d+)/$', views.read_story),
-    # url(r'^(?P<story_id>\d+)/(?P<city_id>\d+)/$', views.read_story),
+    url(r'^(?P<story_id>\d+)/(?P<city_id>\d+)/$', views.read_story),
+    #   <a href="/strories/city={{cityId}}/">
 ]
